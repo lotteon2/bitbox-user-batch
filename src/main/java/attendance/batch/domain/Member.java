e51fd8da505193d20656df3b512d1b3600a309dd
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-10-04 20:43:24 +0900

    [FIX] - Generated 속성 추가

--- src/main/java/attendance/batch/domain/Member.java
+++ src/main/java/attendance/batch/domain/Member.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Getter
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private String memberId;
 
